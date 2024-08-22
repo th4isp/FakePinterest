8b84aca4edf35be159a1abb348ab2cbe9c08a541
Thaís Prates <168002669+th4isp@users.noreply.github.com>
2024-08-22 15:20:53 -0300

    Primeiro commit: Adicionando projeto FakePinterest

---
+++ fakepinterest/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SECRET_KEY"] = "a3190c71717b80582c2b580d8bc02528"
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
+
+from fakepinterest import routes
